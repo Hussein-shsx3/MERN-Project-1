37b122f43b2d5c912693cc4939b53aec591c9c76
Hussein-shsx3 <husseinshsx3@gmail.com>
2024-07-06 18:51:52 +0300

    edit port

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://mern-project-1-frontend.onrender.com"],
     credentials: true,
   })
 );
