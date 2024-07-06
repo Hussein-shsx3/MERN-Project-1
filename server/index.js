838ad553d1c36eb82877d4e607b437c7052b143b
Hussein-shsx3 <husseinshsx3@gmail.com>
2024-07-06 17:18:13 +0300

    edir port

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://mern-project-1-zpur.onrender.com"],
     credentials: true,
   })
 );
